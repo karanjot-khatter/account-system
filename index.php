aa860961c3775f1a25c77b28f88a3cd867862127
karanjot <karanjot_khatter@hotmail.co.uk>
2021-08-08 16:06:23 +0100

    Created sign in card with bootsrap 5.

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Account System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+</head>
+<body>
+<div class="container">
+<!--    Login form start-->
+
+    <div class="row justify-content-center" id="login-box">
+        <div class="col-md-10">
+            <div class="card-group">
+                <div style="flex-grow: 1.5" class="card">
+                    <h1 class="text-center sign-in text-primary">Sign in to Account</h1>
+                    <form action="#" method="POST" id="login-form">
+                        <div class="input">
+                            <span class="icon-background">
+                                <i class="far fa-envelope fa-lg"></i>
+                            </span>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
+                        </div>
+
+                        <div class="input">
+                            <span class="icon-background">
+                               <i class="fas fa-key fa-lg"></i>
+                            </span>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+                        </div>
+
+                        <div class="login-extras">
+                            <div class="remember-me">
+                                <input type="checkbox" name="rem" id="customCheck">
+                                <label for="customCheck">Remember me</label>
+                            </div>
+                            <div class="forgot-password">
+                                <a href="#" id="forgot-link">Forgot Password?</a>
+                            </div>
+                        </div>
+
+                        <input type="submit" Value="Sign in" id="login-btn" class="btn btn-primary login-btn">
+
+                    </form>
+                </div>
+
+                <div style="background-color: #ff8c00;" class="card">
+                    <h1 class="text-center sign-up-title">Hello friends!</h1>
+                    <h4 class="sign-up-body">Enter your personal details and start your journey with us!</h4>
+                    <button id="register-link" class="btn register-btn">Sign up</button>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<!--    Login form end-->
+</div>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</body>
+</html>
