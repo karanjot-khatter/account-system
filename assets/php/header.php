4a1e07ac5a6082f8c67788f0523406c9c3a31993
karanjot <karanjot_khatter@hotmail.co.uk>
2021-08-12 15:33:15 +0100

    created the navigation menu of the home page.

---
+++ assets/php/header.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'assets/php/session.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo ucfirst(basename($_SERVER['PHP_SELF'],'.php')); ?> | Karanjot</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><i class="fas fa-code fa-lg"></i>&nbsp;&nbsp;Karanjot</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'home.php' ? 'active' : '' ?> >" aria-current="page" href="home.php"><i class="fas fa-home"></i>&nbsp;Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'profile.php' ? 'active' : '' ?> " href="profile.php"><i class="fas fa-user-circle"></i>&nbsp;Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'feedback.php' ? 'active' : '' ?>" href="feedback.php" ><i class="fas fa-comment-dots"></i>&nbsp;Feedback</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'notification.php' ? 'active' : '' ?>" href="notification.php"><i class="fas fa-bell"></i>&nbsp;Notification</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-cog"></i>&nbsp;Hi! <?php echo $fname; ?>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#"><i class="fas fa-cog"></i>Setting</a></li>
+                        <li><a class="dropdown-item" href="assets/php/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</body>
+</html>
--- assets/php/session.php
+++ assets/php/session.php
@@ -12,3 +12,15 @@ if (!isset($_SESSION['user']))
 $cEmail = $_SESSION['user'];
 
 $data = $cUser->currentUser($cEmail);
+
+$cid = $data['id'];
+$cname = $data['name'];
+$cpass = $data['password'];
+$cphone = $data['phone'];
+$cgender = $data['gender'];
+$cdob = $data['dob'];
+$cphoto = $data['photo'];
+$created = $data['created_at'];
+$verified = $data['verified'];
+
+$fname = strtok($cname, ' ');
